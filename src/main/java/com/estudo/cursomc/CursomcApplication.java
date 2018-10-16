049f6ea2321fd233e59c009131d7af047c55cb35
Alan <alanlambari@gmail.com>
2018-10-16 14:34:36 -0300

    Tratando exceções adequadamente

--- src/main/java/com/estudo/cursomc/CursomcApplication.java
+++ src/main/java/com/estudo/cursomc/CursomcApplication.java
@@ -1,11 +1,8 @@
 package com.estudo.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.estudo.cursomc.services.S3Service;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
